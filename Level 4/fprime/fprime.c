890d81e35d682ab080fc5aaf5d441891af979ca2
Jose Antonio Ceron Gutierrez <jceron-g@c2r3s3.42malaga.com>
2024-03-14 12:33:18 +0100

    Exercises added

--- Level 4/fprime/fprime.c
+++ Level 4/fprime/fprime.c
@@ -1,37 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 void ft_fprime(int number)
 {
-    int i;
-    int aux;
+	int	i;
+	int	aux;
 
-    aux = number;
-    i = 2;
-    if(aux == 1)
-        printf("1");
-    while (aux > 1)
-    {
-        if(aux % i == 0)
-        {
-            if (aux != number)
-                printf("%s", "*");
-            aux = aux/i;
-            printf("%d", i);
-        }
-        else
-            i++;
-    }
+	aux = number;
+	i = 2;
+	if(aux == 1)
+		printf("1");
+	while(aux > 1)
+	{
+		if(aux % i == 0)
+		{
+			if(aux != number)
+				printf("%s", "*");
+			aux = aux / i;
+			printf("%d", i);
+		}
+		else
+			i++;
+	}
 }
 
 int main(int argc, char **argv)
 {
-    int number;
+	int number;
 
-    number = atoi(argv[1]);
-    if (argc == 2)
-    {
-        ft_fprime(number);
-    }
-    printf("\n");
+	if (argc == 2)
+	{
+		number = atoi(argv[1]);
+		ft_fprime(number);
+	}
+	printf("\n");
 }
+
