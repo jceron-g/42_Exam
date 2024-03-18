174b5d70e00c15e13506943a969361586d7db7c6
Jose Antonio Ceron Gutierrez <jceron-g@c2r3s4.42malaga.com>
2024-03-18 12:42:08 +0100

    Ex added

--- Level 2/inter.c
+++ Level 2/inter.c
@@ -1,32 +1,49 @@
 #include <unistd.h>
 
-int	iter(char *str, char c, int len)
+int first_char(char *string, char c)
 {
-	int	i;
+	int i;
 
 	i = 0;
-	while(str[i] && (i < len || len == -1))
+	while(string[i])
 	{
-		if(str[i] == c)
-			return(1);
+		if(string[i] == c)
+			return(i);
 		i++;
+	
 	}
-	return (0);
+	return(i);
 }
 
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	int	i;
-
+	int i;
+	int	j;
+	int check;
+	
+	check = 0;
+	j = 0;
 	i = 0;
 	if(argc == 3)
 	{
 		while(argv[1][i])
 		{
-			if(!iter(argv[1], argv[1][i], i) && iter(argv[2], argv[1][i], -1))
-				write(1, &argv[1][i], 1);
+			if(i == first_char(argv[1], argv[1][i]))
+			{
+				check = 0;
+				j = 0;
+				while(argv[2][j])
+				{
+					if(argv[1][i] == argv[2][j] && check == 0)
+					{
+						write(1, &argv[1][i], 1);
+						check = 1;
+					}
+					j++;
+				}	
+			}	
 			i++;
 		}
 	}
-	write(1, "\n", 1);
+		write(1, "\n", 1);
 }
---
+++ Level 3/lcm.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+unsigned int lcm(unsigned int a, unsigned int b)
+{
+	unsigned int n;
+
+	if(a == 0 || b == 0)
+		return(0);
+	if(a > b)
+		n = a;
+	else
+		n = b;
+	while(1)
+	{
+		if(n % a == 0 && n % b == 0)
+			return(n);
+		n++;
+	}
+}
+
+int main(void)
+{
+	unsigned int number;
+
+	number = lcm(7, 4);
+	printf("%d", number);
+}
