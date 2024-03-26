db88ba9527148a56dc98599c62af51c3d44763e3
Jose Antonio Ceron Gutierrez <jceron-g@c1r3s2.42malaga.com>
2024-03-26 16:54:27 +0100

    More added

--- Level 2/union.c
+++ Level 2/union.c
@@ -1,44 +1,39 @@
 #include <unistd.h>
 
-int check(int c, char *str, int index)
-{
-	int	i;
-
-	i = 0;
-	while(i < index)
-	{
-		if(str[i] == c)
-			return(0);
-		i++;
-	}
-	return(1);
-}
-
 int main(int argc, char **argv)
 {
+	char tab[256] = {0};
 	int	i;
 	int j;
-	int	k;
 
 	i = 0;
-	j = 0;
-	k = 0;
+	j = 1;
 	if(argc == 3)
 	{
-		while(argv[1][i])
-			i++;
-		while(argv[2][j])
+		while(j < 3)
 		{
-			argv[1][i] = argv[2][j];
-			i++;
+			while(argv[j][i])
+			{
+				tab[(int)argv[j][i]] = 1;
+				i++;
+			}
+			i = 0;
 			j++;
 		}
-		i--;
-		while(k <= i)
+		j = 1;
+		while(j < 3)
 		{
-			if(check(argv[1][k], argv[1], k) == 1)
-				write(1, &argv[1][k], 1);
-			k++;
+			while(argv[j][i])
+			{
+				if(tab[(int)argv[j][i]] == 1)
+				{
+					write(1, &argv[j][i], 1);
+					tab[(int)argv[j][i]] = 2;
+				}
+				i++;
+			}
+			i = 0;
+			j++;
 		}
 	}
 	write(1, "\n", 1);
