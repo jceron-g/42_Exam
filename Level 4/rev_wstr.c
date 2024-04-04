fd7dbc91de546f428443ee8dbe5fb373ceb3ff73
Jose Antonio Ceron Gutierrez <jceron-g@c1r9s5.42malaga.com>
2024-04-04 09:17:01 +0200

    More ex

--- Level 4/rev_wstr.c
+++ Level 4/rev_wstr.c
@@ -8,12 +8,12 @@ int main(int argc, char **argv)
 	int i = 0;
 		
 	if(argc == 2)
-	{   
-		while(argv[1][i] != '\0')
+	{
+		while(argv[1][i])
 			i++;
 		while(i >= 0)
 		{
-			while(argv[1][i] == '\0' || argv[1][i] == ' ' || argv[1][i] == '\t')
+			while(argv[1][i] == '\0' || argv[1][i]  == ' ' || argv[1][i] == '\t')
 				i--;
 			end = i;
 			while(argv[1][i] && argv[1][i] != ' ' && argv[1][i] != '\t')
@@ -22,14 +22,15 @@ int main(int argc, char **argv)
 			flag = start;
 			while(start <= end)
 			{
-				write (1, &argv[1][start],1);
-				start++;		
+				write(1, &argv[1][start], 1);
+				start++;
 			}
-			if (flag !=0)
+			if(flag != 0)
 			{
 				write(1, " ", 1);
 			}
+
 		}
+		write(1, "\n", 1);
 	}
-	write(1, "\n", 1);
 }
